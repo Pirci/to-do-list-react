b5034fbc2fdab07d99c0e1703296457d0ceb4316
Pirci <emrepirci@gmail.com>
2021-10-28 17:14:48 +0200

    State functions done

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,46 @@
-import React, { Component } from "react";
-import _ from "lodash";
-
+import React, { useState } from "react";
 import "./App.css";
-import NewTaskGenerator from "./components/new-task-generator";
-import ListOfTasks from "./components/list-of-tasks";
+// Importing Components
+import Form from './components/form'
+import TodoList from './components/TodoList'
+// import NewTaskGenerator from "./components/new-task-generator";
+// import ListOfTasks from "./components/list-of-tasks";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listOfTasks: [],
-      listOfCompleted: [],
-    };
-  }
-
-  updateListOfTasks = (newTask) => {
-    this.setState({ listOfTasks: [...this.state.listOfTasks, newTask] });
-  };
-
-  render() {
-    return (
-      <div className='my-container'>
-        <NewTaskGenerator updateListOfTasks={this.updateListOfTasks} />
-        <ListOfTasks listOfTasks={this.state.listOfTasks} />
-      </div>
-    );
-  }
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>TO-DO List App</h1>
+      </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList />
+    </div>
+  );
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       listOfTasks: [],
+//       listOfCompleted: [],
+//     };
+//   }
+
+//   updateListOfTasks = (newTask) => {
+//     this.setState({ listOfTasks: [...this.state.listOfTasks, newTask] });
+//   };
+
+//   render() {
+//     return (
+//       <div className='my-container'>
+//         <NewTaskGenerator updateListOfTasks={this.updateListOfTasks} />
+//         <ListOfTasks listOfTasks={this.state.listOfTasks} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = () => {
+    return (
+        <div className="todo"></div>
+        <li className="todo-item">HEYYY</li>
+        <button className="fas fa-check"><i></i></button>
+        <button className="fas fa-trash"></button>
+    );
+}
+
+export default Todo;
